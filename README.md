cb10d1a8fd004866e8bbfceadb8d42d6caaeeda6
Main User Account <resler@ualberta.ca>
2017-01-23 00:31:59 -0700

    added README and updated server.py

--- README.md
+++ README.md
@@ -16,6 +16,16 @@ server.py contains contributions from:
 
 * Abram Hindle
 * Eddie Antonio Santos
+* Connor Resler
+* Ryan Satyabrata - I read his code found here: https://github.com/kobitoko/CMPUT404-assignment-webserver/blob/master/server.py#L45 to help me understand how to approach aspects of the assignment. I also used some code directly from here, which is referenced in my code 
+
+I referenced the following webpages for help with understanding concepts and how to write code using certain functions:
+https://docs.python.org/2/library/time.html#module-time
+https://docs.python.org/2/library/os.path.html
+https://docs.python.org/2/library/mimetypes.html
+https://docs.python.org/2/library/socketserver.html#SocketServer.BaseRequestHandler
+https://docs.python.org/3/library/socketserver.html
+https://github.com/enthought/Python-2.7.3/blob/master/Lib/SimpleHTTPServer.py
 
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
--- server.py
+++ server.py
@@ -1,7 +1,13 @@
 #  coding: utf-8 
 import SocketServer
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+import os.path
+
+import time
+
+import mimetypes
+
+# Copyright 2016 Abram Hindle, Eddie Antonio Santos, Connor Resler, Ryan Satyabrata
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,11 +34,79 @@ import SocketServer
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
-    
+
+    requestHeader = []
+    responseHeader = ""
+    body = ""
+    tPath = ""
+
+    def handleFile(self, path):
+        #Open file, excluding leading '/'
+        f = open(path[1:], 'r')
+        
+        #The extenstion that will tell the file type will be in the portion
+        #of the URI following the last '/'
+        temp = path.split('/')
+        #https://docs.python.org/2/library/mimetypes.html
+        ctype = mimetypes.guess_type(temp[-1])
+        
+        self.responseHeader += "HTTP/1.1 200 OK\r\n"
+        self.responseHeader += "Content-Type: " + ctype[0] + "\r\n"
+        
+        #Code adapted from github user cournape from 
+        #https://github.com/enthought/Python-2.7.3/blob/master/Lib/SimpleHTTPServer.py
+        #Get length of content
+        fs = os.fstat(f.fileno())
+        self.responseHeader += "Content-Length: " + str(fs[6]) + "\r\n"
+        self.body = f.read()
+
+    #Some code here was written by Ryan Satyabrata under Apache-2.0 License
+    #Taken from https://github.com/kobitoko/CMPUT404-assignment-webserver/blob/master/server.py#L45 on January 21, 2017
+    def findPath(self, headerList):
+        path = "/www/"
+
+        if(len(headerList) > 1):
+            path += headerList[1]
+
+        #https://docs.python.org/2/library/os.path.html
+        #Get path
+        path = os.path.abspath(path)
+
+        #Check if current path is a directory and add '/index.html'
+        if(os.path.isdir(os.curdir + path)):
+            path += "/index.html"
+
+        #Check if current path is a file and handle it
+        if(os.path.isfile(os.curdir + path)):
+            self.handleFile(path)
+
+        #Handle file not found. These lines were written by Ryan Satyabrata
+        #Under Apache-2.0 License, taken from https://github.com/kobitoko/CMPUT404-assignment-webserver/blob/master/server.py#L45 on January 21, 2017
+        else:
+            self.responseHeader += "HTTP/1.1 404 NOT FOUND\r\n"
+            self.responseHeader += "Content-Type: text/html;\r\n"
+            self.body = "<html><head></head><body><h1>404 NOT FOUND</h1></body></html>"
+        self.tPath = path
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+        requestHeader = self.data.split()
+
+        if(requestHeader[0].upper() == "GET"):
+            self.findPath(requestHeader)
+        else:
+            self.responseHeader += "HTTP/1.1 405 Method Not Allowed\r\n"
+
+        #https://docs.python.org/2/library/time.html#module-time
+        #Get current time and format it
+        currentTime = time.localtime()
+        currentTime = time.strftime("Date: %d %b %Y %H:%M:%S\r\n", currentTime)
+        self.responseHeader += currentTime;
+        #Send header and body
+        self.request.sendall(self.responseHeader + "\r\n" + self.body)
+
+        #Print information that looks like what SimpleHTTPServer does
+        print(self.client_address[0] + " - " + currentTime[:-4] + " - " + self.tPath)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -44,3 +118,5 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
+
